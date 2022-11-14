ea2c5f14d06fe899fbc3ff937ad8f58f7556330e
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-11-14 17:51:14 +0000

    Aula 5: Upload incomplete queue solution

--- P05/lQueue.h
+++ P05/lQueue.h
@@ -13,33 +13,79 @@ template <typename T>
 class lQueue
 {
   private:
-  public:
-    lQueue(void)
+    class sNode
     {
+      public:
+        T value;     // the data stored in the node
+        sNode *next; // pointer to the next node
+    };
+    int n_nodes;     // number of nodes
+    int max_size;
+    sNode *head;     // head of the queue
+    sNode *tail;     // tail of the queue
+  
+  public:
+    lQueue(int n = 100)
+    {
+      n_nodes = 0;
+      max_size = n;
+      head = nullptr;
+      tail = nullptr;
+      pos = -1; // default position is before the head of the list
     }
+
     ~lQueue(void)
     {
+      sNode *sn1,*sn2;
+
+      for(sn1 = head;sn1 != nullptr;sn1 = sn2)
+      {
+        sn2 = sn1->next;
+        delete sn1;
+      }
+      n_nodes = 0;
+      head = nullptr;
+      tail = nullptr;
     }
+
     void clear(void)
     {
+      for(sn1 = head;sn1 != nullptr;sn1 = sn2)
+      {
+        sn2 = sn1->next;
+        delete sn1;
+      }
+      n_nodes = 0;
     }
+
     int size(void) const
     {
+      return n_nodes;
     }
+
     int is_full(void) const
     {
+      return (n_nodes >= max_size) ? 1 : 0;
     }
+
     int is_empty(void) const
     {
+      return (n_nodes == 0) ? 1 : 0;
     }
+
     void enqueue(T &v)
     {
     }
+
     T dequeue(void)
     {
     }
+
     T peek(void)
     {
+      sNode *sn1;
+      sn1 = head;
+      return sn1->value;
     }
 };
 
